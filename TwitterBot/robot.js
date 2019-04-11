6afc41b0237f22239a891c1be98bb7f9e21386f2
Erik Remington Lee <eriklee19@gmail.com>
2019-04-10 20:50:59 -0400

    working with JSON

--- TwitterBot/robot.js
+++ TwitterBot/robot.js
@@ -3,17 +3,26 @@ console.log("Bot Initialized");
 var config = require('./config');
 var Twit = require('twit');
 
-var tweet = new Twit(config);
+var T = new Twit(config);
 
 var params = {
-    q: 'rainbow since:2017-12-11',
-    count: 10
+    q: 'rainbow since:2016-12-11',
+    count: 20
 };
 
-console.log(tweet.type)
-tweet.get('search/tweets', params, getData);
+
+T.get('search/tweets', params, getData);
 
 function getData(err, data, response) {
-    console.log(data);
+    var tweet = data.statuses;
+    var x =1;
+    
+    for (let i=0; i< tweet.length ; i++) {
+
+    
+    console.log(tweet[i].text)
+    console.log(x)
+    x += 1;
+    };
 }
 
